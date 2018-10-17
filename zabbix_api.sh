8b81e622ae81690cb18cb76e30e5b288b4ffbd2d
Bernardo Falese <bfalese@navent.com>
2018-10-17 15:35:43 -0300

    https

--- zabbix_api.sh
+++ zabbix_api.sh
@@ -9,14 +9,14 @@ echo HOSTIP $HOSTIP
 LOGIN_BODY='{"jsonrpc":"2.0","method":"user.login","params":{ "user":"deploy_bot","password":"deploy"},"auth":null,"id":0}'
 echo $LOGIN_BODY
 TOKEN=$(curl -X POST -H 'Content-type:application/json' -d "$LOGIN_BODY" \
--s -N "http://zabbix.navent.com/api_jsonrpc.php" | jq -r '.result')
+-s -N "https://zabbix.navent.com/api_jsonrpc.php" | jq -r '.result')
 echo TOKEN $TOKEN
 
 HOSTID_BODY='{"jsonrpc":"2.0","method":"host.get","params":{"filter":{"host":["'"$HOSTNAME"'"]}},"auth":"'"$TOKEN"'","id":1}'
 echo HOSTID_BODY $HOSTID_BODY
 
 HOSTID=$(curl -X POST -H 'Content-type:application/json' -d "$HOSTID_BODY" \
--s -N "http://zabbix.navent.com/api_jsonrpc.php" | jq -r '.result[0].hostid')
+-s -N "https://zabbix.navent.com/api_jsonrpc.php" | jq -r '.result[0].hostid')
 echo HOSTID $HOSTID
 
 case $1 in
@@ -48,19 +48,19 @@ case $1 in
 		CREATE_BODY='{"jsonrpc":"2.0","method":"host.create","params":{"host":"'"$HOSTNAME"'","templates":['"${TEMPLATE_IDS::-1}"'],"groups":['"${GROUP_IDS::-1}"'],"interfaces":[{"type":1,"main":1,"useip":1,"ip":"'"$HOSTIP"'","dns":"","port":"10050"}'"$JMX_INTERFACE"']},"auth":"'"$TOKEN"'","id":3}'
 		echo CREATE_BODY $CREATE_BODY
 
-		curl -X POST -H 'Content-type:application/json' -d "$CREATE_BODY" -s -N "http://zabbix.navent.com/api_jsonrpc.php"
+		curl -X POST -H 'Content-type:application/json' -d "$CREATE_BODY" -s -N "https://zabbix.navent.com/api_jsonrpc.php"
 		;;
 	enable)
 		UPDATE_BODY='{"jsonrpc":"2.0","method":"host.update","params":{"hostid":"'"$HOSTID"'","status":0},"auth":"'"$TOKEN"'","id":3}'
 		echo UPDATE_BODY $UPDATE_BODY
 
-		curl -X POST -H 'Content-type:application/json' -d "$UPDATE_BODY" -s -N "http://zabbix.navent.com/api_jsonrpc.php"
+		curl -X POST -H 'Content-type:application/json' -d "$UPDATE_BODY" -s -N "https://zabbix.navent.com/api_jsonrpc.php"
 		;;
 	disable)
 		UPDATE_BODY='{"jsonrpc":"2.0","method":"host.update","params":{"hostid":"'"$HOSTID"'","status":1},"auth":"'"$TOKEN"'","id":3}'
 		echo UPDATE_BODY $UPDATE_BODY
 
-		curl -X POST -H 'Content-type:application/json' -d "$UPDATE_BODY" -s -N "http://zabbix.navent.com/api_jsonrpc.php"
+		curl -X POST -H 'Content-type:application/json' -d "$UPDATE_BODY" -s -N "https://zabbix.navent.com/api_jsonrpc.php"
 		;;
 	*)
 		echo "desconocido, debe ser uno de (create,enable,disable)"
